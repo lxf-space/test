2eb81d158e816ac68a5df8c5496f48bb35c933e1
lxf-space <53386502+lxf-space@users.noreply.github.com>
2019-11-24 19:15:37 +0800

    Update Demo01.java

--- demo01/src/package1/Demo01.java
+++ demo01/src/package1/Demo01.java
@@ -4,6 +4,6 @@ public class Demo01 {
     public static void main(String[] args) {
         System.out.println("主干");
         System.out.println("11111111111111");
-        System.out.println("01010101");
+        System.out.println("010101011111111111111111111111111111111111111");
     }
 }
