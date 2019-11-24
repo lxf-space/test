fd03b1014ad43352e99639f467809b96b2f41231
lxf <1021039951@qq.com>
2019-11-24 19:26:39 +0800

    fenzhi 2

--- demo01/src/package1/Demo01.java
+++ demo01/src/package1/Demo01.java
@@ -3,8 +3,6 @@ package package1;
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("主干");
-        System.out.println("11111111111111");
-        System.out.println("010101011111111111111111111111111111111111111");
-        System.out.println("222222222222222222");
+        System.out.println("fenzhi 2");
     }
 }
