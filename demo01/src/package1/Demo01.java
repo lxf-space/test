a7038ea525c13d8c6a14c6d66dae8fb68106ea37
lxf <1021039951@qq.com>
2019-11-26 20:17:36 +0800

    删除1

--- demo01/src/package1/Demo01.java
+++ demo01/src/package1/Demo01.java
@@ -3,7 +3,7 @@ package package1;
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("主干");
-        System.out.println("fenzhi 2");
+
         System.out.println("fenzhi1");
     }
 }
